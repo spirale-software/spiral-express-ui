0188778b54920bd789f3f4aa0ab791acd1f2c327
LapiEmo <lapigerard@yahoo.fr>
2020-09-02 04:38:33 +0200

    Structure d'envoi de coli

--- src/app/pages/envoi/envoi.component.ts
+++ src/app/pages/envoi/envoi.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {MenuItem} from "primeng";
 
 @Component({
     selector: 'app-envoi',
@@ -6,7 +7,29 @@ import {Component, OnInit} from "@angular/core";
 })
 export class EnvoiComponent implements OnInit {
 
-    constructor() {}
+    breadcrumbItems: MenuItem[];
+
+    envois: any[];
+
+    constructor() {
+        this.breadcrumbItems = [];
+        this.breadcrumbItems.push({label: 'envois de coli'});
+        this.envois = [
+            {
+                etatColi: 'En Cours',
+                dateEnvoi: '10/10/2020',
+                emetteur: 'Yannick',
+                volumeColi: '0.33',
+                destinataire: 'Jules Lefranc',
+                paysExpedition: 'Belgique',
+                paysDestination: 'Rwanda',
+                villeDestination: 'Kigali',
+                adresseDestination: 'Kigali centre',
+                montantTotalTransaction: 50.00
+            },
+
+        ];
+    }
 
     ngOnInit(): void {
     }
