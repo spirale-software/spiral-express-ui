b0f9504435fe32a494768124f4e8dbc7d8a16a7a
LapiEmo <lapigerard@yahoo.fr>
2020-09-02 05:25:32 +0200

    Encodage nouvel envoi

--- src/app/pages/envoi/envoi-update.component.ts
+++ src/app/pages/envoi/envoi-update.component.ts
@@ -1,10 +1,19 @@
 import {Component} from "@angular/core";
+import {MenuItem} from "primeng";
 
 @Component({
     selector: 'app-envoi',
     templateUrl: './envoi-update.component.html'
 })
 export class EnvoiUpdateComponent {
+
+    breadcrumbItems: MenuItem[];
+
+    constructor() {
+        this.breadcrumbItems = [];
+        this.breadcrumbItems.push({label: 'envois de coli'});
+        this.breadcrumbItems.push({label: 'encodage nouvel envoi'});
+    }
 
 }
 
