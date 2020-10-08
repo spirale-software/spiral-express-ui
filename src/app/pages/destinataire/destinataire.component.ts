b1a4d94a83981cd0e628bd96967333d8b84d46d4
LapiEmo <lapigerard@yahoo.fr>
2020-10-08 22:15:49 +0200

    Destinataires

--- src/app/pages/destinataire/destinataire.component.ts
+++ src/app/pages/destinataire/destinataire.component.ts
@@ -1,5 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, Optional} from "@angular/core";
 import {DynamicDialogRef} from "primeng";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-destinataire',
@@ -9,15 +10,22 @@ export class DestinataireComponent {
 
     destinataires: any[];
 
-    constructor(private ref: DynamicDialogRef) {
+    constructor(@Optional() private ref: DynamicDialogRef, private router: Router) {
         this.destinataires = [
-            {nom: 'Gérard', prenom: 'Yannick', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Cameroun', adresse: 'Douala Bassa'},
-            {nom: 'Symbol', prenom: 'Yvano', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Tchad', adresse: 'Djamena Lac'},
-            {nom: 'Stephen', prenom: 'Gustave', telephone: '00336989521', email: 'gustave@gmail.com', pays: 'Senegal', adresse: 'Dakar Centre'},
+            {id: 1, nom: 'Gérard', prenom: 'Yannick', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Cameroun', adresse: 'Douala Bassa'},
+            {id: 2, nom: 'Symbol', prenom: 'Yvano', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Tchad', adresse: 'Djamena Lac'},
+            {id: 3, nom: 'Stephen', prenom: 'Gustave', telephone: '00336989521', email: 'gustave@gmail.com', pays: 'Senegal', adresse: 'Dakar Centre'},
         ];
     }
 
     selectPartenaire(partenaire): void {
         this.ref.close(partenaire);
+    }
+
+    navigate(destinataire) {
+        console.log('window.innerWidth: ', window.innerWidth);
+        if (window.innerWidth < 650) {
+            this.router.navigate(['destinataires', destinataire.id, 'modifier']);
+        }
     }
 }
--- src/app/pages/destinataire/destinataire.route.ts
+++ src/app/pages/destinataire/destinataire.route.ts
@@ -3,7 +3,7 @@ import {DestinataireComponent} from './destinataire.component';
 import {DestinataireUpdateComponent} from './destinataire-update.component';
 
 export const DESTINATAIRE_ROUTES: Routes = [
-    {path: 'destinataires', component: DestinataireComponent},
-    {path: 'destinataires/:id/modifier', component: DestinataireUpdateComponent},
-    {path: 'destinataires/creer', component: DestinataireUpdateComponent}
+    { path: 'destinataires', component: DestinataireComponent },
+    { path: 'destinataires/:id/modifier', component: DestinataireUpdateComponent },
+    { path: 'destinataires/creer', component: DestinataireUpdateComponent }
 ];
