0fcea9ea4d2e58f0fb376b3645f5eebbee230f7d
LapiEmo <lapigerard@yahoo.fr>
2020-10-10 23:25:29 +0200

    Partenaire update

--- src/app/pages/partenaire/partenaire.component.ts
+++ src/app/pages/partenaire/partenaire.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from "@angular/core";
-import {MenuItem} from "primeng";
+import {Component, OnInit, Optional} from "@angular/core";
+import {DynamicDialogRef, MenuItem} from "primeng";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-partenaire',
@@ -8,19 +9,40 @@ import {MenuItem} from "primeng";
 export class PartenaireComponent implements OnInit {
 
     breadcrumbItems: MenuItem[];
+
     partenaires: any[];
 
-    constructor() {
+    isOpenLikeDialog = false;
+
+    constructor(private router: Router, @Optional() private ref: DynamicDialogRef ) {
         this.breadcrumbItems = [];
         this.breadcrumbItems.push({label: 'Partenaires'});
         this.partenaires = [
-            {nom: 'Gérard', prenom: 'Yannick', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Cameroun', adresse: 'Douala Bassa'},
-            {nom: 'Symbol', prenom: 'Yvano', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Tchad', adresse: 'Djamena Lac'},
-            {nom: 'Stephen', prenom: 'Gustave', telephone: '00336989521', email: 'gustave@gmail.com', pays: 'Senegal', adresse: 'Dakar Centre'},
+            {id: 1, nom: 'Gérard', prenom: 'Yannick', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Cameroun', adresse: 'Douala Bassa'},
+            {id: 2, nom: 'Symbol', prenom: 'Yvano', telephone: '045888888', email: 'contact@lapiemo.com', pays: 'Tchad', adresse: 'Djamena Lac'},
+            {id: 3, nom: 'Stephen', prenom: 'Gustave', telephone: '00336989521', email: 'gustave@gmail.com', pays: 'Senegal', adresse: 'Dakar Centre'},
         ];
+
+        if (this.ref) {
+            this.isOpenLikeDialog = true;
+        }
     }
 
     ngOnInit(): void {
+    }
+
+    showUpdate(partenaire): void {
+        if (window.innerWidth <= 650) {
+            this.router.navigate(['/partenaires', partenaire.id, 'modifier']);
+        }
+    }
+
+    selectPartenaire(partenaire): void {
+
+    }
+
+    confirmerSuppression(partenaire) {
+
     }
 }
 
--- src/app/pages/partenaire/partenaire.module.ts
+++ src/app/pages/partenaire/partenaire.module.ts
@@ -3,9 +3,10 @@ import {PartenaireComponent} from './partenaire.component';
 import {PartenaireUpdateComponent} from './partenaire-update.component';
 import {SharedModule} from '../shared/shared.module';
 import {PrimengLibModule} from '../shared/primeng-lib/primeng-lib.module';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
-    imports: [SharedModule, PrimengLibModule],
+    imports: [SharedModule, PrimengLibModule, CommonModule],
     exports: [],
     declarations: [PartenaireComponent, PartenaireUpdateComponent]
 })
